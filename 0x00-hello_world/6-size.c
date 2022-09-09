c4ed643dddcf1b2b4a38a10b508d62b9aaa93c55
aCodingXhosaBaddie <yamkelanip@gmail.com>
2022-09-08 19:40:05 -0700

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
-
-
 /**
  * main - Entry point
  *
  *Return: Always 0
  */
 
-
 int main(void)
-
 {
 	char acharacter;
 	int ainteger;
@@ -25,5 +21,4 @@ int main(void)
 	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 
         return (0);
-
 }

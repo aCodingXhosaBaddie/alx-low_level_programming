a7be21a675963aa149d907bc4ca8afdcd6857219
aCodingXhosaBaddie <yamkelanip@gmail.com>
2022-09-14 19:40:11 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 /**
  * main - prints putchar
  * Return: Always (0)
- * */
+ */
 int main(void)
 {
 	_putchar('_');
@@ -15,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
 	return (0);
 }
